0ce44b3759011ad726f1fec4c5a48bdc24ccd16a
Nanioi <0802ny@gmail.com>
2021-08-12 15:42:33 +0900

    미세먼지 측정 앱 프로젝트
    - LocationManager
    - Retrofit2
    - Coroutine
    - App Widgets
    - 위치 정보 가져와 오픈 API로 부터 미세먼지 정보를 가져오고 홈스크린에 미세멎지 위젯을 추가할 수 있다.

---
+++ check_fine_dust/app/src/main/java/com/nanioi/check_fine_dust/data/models/airquality/Response.kt
@@ -0,0 +1,11 @@
+package com.nanioi.check_fine_dust.data.models.airquality
+
+
+import com.google.gson.annotations.SerializedName
+
+data class Response(
+    @SerializedName("body")
+    val body: Body? = null,
+    @SerializedName("header")
+    val header: Header? = null
+)
---
+++ check_fine_dust/app/src/main/java/com/nanioi/check_fine_dust/data/models/monitoringstation/Header.kt
@@ -0,0 +1,11 @@
+package com.nanioi.check_fine_dust.data.models.monitoringstation
+
+
+import com.google.gson.annotations.SerializedName
+
+data class Header(
+    @SerializedName("resultCode")
+    val resultCode: String?,
+    @SerializedName("resultMsg")
+    val resultMsg: String?
+)
